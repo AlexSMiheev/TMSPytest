7a530dc4092912103ac0e80b36e3564c7dab9a4a
AlexSMiheev <alexsmiheev@hotmail.com>
2023-02-21 16:06:42 +0300

    add dz21

--- google.py
+++ google.py
@@ -1,9 +1,19 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-def test_google():
-    chrome = webdriver.Chrome()
+@pytest.fixture
+def get_driver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    yield driver
+    driver.close()
+
+
+def test_google(get_driver):
+    chrome = get_driver
     url = 'http://google.com/'
     chrome.get(url=url)
     chrome.maximize_window()
@@ -11,4 +21,3 @@ def test_google():
     search_box.send_keys('python 3.10')
     search_box.submit()
     assert 'python 3.10' in chrome.title
-    chrome.close()
